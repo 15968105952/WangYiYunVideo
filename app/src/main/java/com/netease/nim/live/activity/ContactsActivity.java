5518fe483139854d53ddbacf7394efe82f2bd0a9
mazhuang123 <1824902907@qq.com>
2017-12-26 12:29:32 +0800

    第二次提交，提交主工程

---
+++ app/src/main/java/com/netease/nim/live/activity/ContactsActivity.java
@@ -0,0 +1,15 @@
+package com.netease.nim.live.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.netease.nim.live.R;
+
+public class ContactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+    }
+}
---
+++ app/src/main/java/com/netease/nim/live/contact/ServerEnvs.java
@@ -0,0 +1,12 @@
+package com.netease.nim.live.contact;
+
+final class ServerEnvs {
+
+    //
+    // ENVs
+    // DEFAULT Env.REL
+    //
+
+    static final ServerConfig.ServerEnv SERVER = ServerConfig.ServerEnv.REL;
+
+}
